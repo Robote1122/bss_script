668955566c3584386b56058f747e6923cdb77c14
Robote1122 <ivan.1m4f@yandex.ru>
2026-07-13 22:35:44 +0300

    Update

--- vme.lua
+++ vme.lua
@@ -3,6 +3,7 @@ local cheat_name = 'nvm'
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Robote1122/bss_script/refs/heads/main/library.lua'))()
 local api = loadstring(game:HttpGet('https://raw.githubusercontent.com/Robote1122/bss_script/refs/heads/main/api.lua'))()
 
+print('start created')
 -- Создаём основное окно
 local Window = library:CreateWindow({
     WindowName = "Kocmoc v3.0.2 Remastered",
@@ -187,6 +188,8 @@ end)
 farmt:CreateToggle("Auto Honeystorm", nil, function(State)
     _G.honeystorm = State
 end)
+
+print('ppp')
 
 -- ============================================
 -- ВКЛАДКА "Combat"
