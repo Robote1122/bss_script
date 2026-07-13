82909fb6b50d51f5a97b18b7f5bba8eb16fa3fba
Robote1122 <ivan.1m4f@yandex.ru>
2026-07-13 22:16:39 +0300

    Update

--- vme.lua
+++ vme.lua
@@ -1,6 +1,5 @@
 local cheat_name = 'nvm'
-local durl='https://raw.githubusercontent.com/Robote1122/bss_script/refs/heads/main/'
-local library = loadstring(game:HttpGet(durl+'library.lua'))()
-local api = loadstring(game:HttpGet(durl+"api.lua"))()
+local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Robote1122/bss_script/refs/heads/main/library.lua'))()
+local api = loadstring(game:HttpGet('https://raw.githubusercontent.com/Robote1122/bss_script/refs/heads/main/api.lua'))()
 
 print(api.ver)
