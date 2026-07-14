898cc2e16d2ff25b5ac308eb015f2f8bcec36f93
Robote1122 <ivan.1m4f@yandex.ru>
2026-07-14 14:33:06 +0300

    Update

--- mve.lua
+++ mve.lua
@@ -185,7 +185,7 @@ table.sort(donatableItemsTable)
 table.sort(buffTable)
 
 -- float pad
---[[
+
 local floatpad = Instance.new("Part", game:GetService("Workspace"))
 floatpad.CanCollide = false
 floatpad.Anchored = true
@@ -444,5 +444,5 @@ AllTokens.ChildRemoved:Connect(function(child)
     if not spawned_tokens[child] return end
     print(spawned_tokens[child].Name,os.clock()-spawned_tokens[child].spawned)
 end)
-]]
+
 print(acces_key)
