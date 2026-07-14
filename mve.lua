27d295a3fe5c1f42cdd622b64bb092e0f02cced9
Robote1122 <ivan.1m4f@yandex.ru>
2026-07-14 15:09:50 +0300

    Update

--- mve.lua
+++ mve.lua
@@ -65,114 +65,114 @@ local Items = require(game:GetService("ReplicatedStorage").EggTypes).GetTypes()
 hives = game.Workspace.Honeycombs:GetChildren() for i = #hives, 1, -1 do  v = game.Workspace.Honeycombs:GetChildren()[i] if v.Owner.Value == nil then game.ReplicatedStorage.Events.ClaimHive:FireServer(v.HiveID.Value) end end
 
 local temptable = {
-    version = "3.0.2",
-    blackfield = "Sunflower Field",
-    redfields = {},
-    bluefields = {},
-    whitefields = {},
-    shouldiconvertballoonnow = false,
-    balloondetected = false,
-    puffshroomdetected = false,
-    magnitude = 70,
-    blacklist = {
-        ""
-    },
-    running = false,
-    configname = "",
-    tokenpath = game:GetService("Workspace").Collectibles,
-    started = {
-        vicious = false,
-        mondo = false,
-        windy = false,
-        ant = false,
-        monsters = false
-    },
-    detected = {
-        vicious = false,
-        windy = false
-    },
-    tokensfarm = false,
-    converting = false,
-    honeystart = 0,
-    grib = nil,
-    gribpos = CFrame.new(0,0,0),
-    honeycurrent = statstable.Totals.Honey,
-    dead = false,
-    float = false,
-    pepsigodmode = false,
-    pepsiautodig = false,
-    alpha = false,
-    beta = false,
-    myhiveis = false,
-    invis = false,
-    windy = nil,
-    sprouts = {
-        detected = false,
-        coords
-    },
-    cache = {
-        autofarm = false,
-        killmondo = false,
-        vicious = false,
-        windy = false
-    },
-    allplanters = {},
-    planters = {
-        planter = {},
-        cframe = {},
-        activeplanters = {
-            type = {},
-            id = {}
-        }
-    },
-    monstertypes = {"Ladybug", "Rhino", "Spider", "Scorpion", "Mantis", "Werewolf"},
-    ["stopapypa"] = function(path, part)
-        local Closest
-        for i,v in next, path:GetChildren() do
-            if v.Name ~= "PlanterBulb" then
-                if Closest == nil then
-                    Closest = v.Soil
-                else
-                    if (part.Position - v.Soil.Position).magnitude < (Closest.Position - part.Position).magnitude then
-                        Closest = v.Soil
-                    end
-                end
-            end
-        end
-        return Closest
-    end,
-    coconuts = {},
-    crosshairs = {},
-    crosshair = false,
-    coconut = false,
-    act = 0,
-    ['touchedfunction'] = function(v)
-        if lasttouched ~= v then
-            if v.Parent.Name == "FlowerZones" then
-                if v:FindFirstChild("ColorGroup") then
-                    if tostring(v.ColorGroup.Value) == "Red" then
-                        maskequip("Demon Mask")
-                    elseif tostring(v.ColorGroup.Value) == "Blue" then
-                        maskequip("Diamond Mask")
-                    end
-                else
-                    maskequip("Gummy Mask")
-                end
-                lasttouched = v
-            end
-        end
-    end,
-    runningfor = 0,
-    oldtool = rtsg()["EquippedCollector"],
-    ['gacf'] = function(part, st)
-        coordd = CFrame.new(part.Position.X, part.Position.Y+st, part.Position.Z)
-        return coordd
-    end
+	version = "3.0.2",
+	blackfield = "Sunflower Field",
+	redfields = {},
+	bluefields = {},
+	whitefields = {},
+	shouldiconvertballoonnow = false,
+	balloondetected = false,
+	puffshroomdetected = false,
+	magnitude = 70,
+	blacklist = {
+		""
+	},
+	running = false,
+	configname = "",
+	tokenpath = game:GetService("Workspace").Collectibles,
+	started = {
+		vicious = false,
+		mondo = false,
+		windy = false,
+		ant = false,
+		monsters = false
+	},
+	detected = {
+		vicious = false,
+		windy = false
+	},
+	tokensfarm = false,
+	converting = false,
+	honeystart = 0,
+	grib = nil,
+	gribpos = CFrame.new(0,0,0),
+	honeycurrent = statstable.Totals.Honey,
+	dead = false,
+	float = false,
+	pepsigodmode = false,
+	pepsiautodig = false,
+	alpha = false,
+	beta = false,
+	myhiveis = false,
+	invis = false,
+	windy = nil,
+	sprouts = {
+		detected = false,
+		coords
+	},
+	cache = {
+		autofarm = false,
+		killmondo = false,
+		vicious = false,
+		windy = false
+	},
+	allplanters = {},
+	planters = {
+		planter = {},
+		cframe = {},
+		activeplanters = {
+			type = {},
+			id = {}
+		}
+	},
+	monstertypes = {"Ladybug", "Rhino", "Spider", "Scorpion", "Mantis", "Werewolf"},
+	["stopapypa"] = function(path, part)
+		local Closest
+		for i,v in next, path:GetChildren() do
+			if v.Name ~= "PlanterBulb" then
+				if Closest == nil then
+					Closest = v.Soil
+				else
+					if (part.Position - v.Soil.Position).magnitude < (Closest.Position - part.Position).magnitude then
+						Closest = v.Soil
+					end
+				end
+			end
+		end
+		return Closest
+	end,
+	coconuts = {},
+	crosshairs = {},
+	crosshair = false,
+	coconut = false,
+	act = 0,
+	['touchedfunction'] = function(v)
+		if lasttouched ~= v then
+			if v.Parent.Name == "FlowerZones" then
+				if v:FindFirstChild("ColorGroup") then
+					if tostring(v.ColorGroup.Value) == "Red" then
+						maskequip("Demon Mask")
+					elseif tostring(v.ColorGroup.Value) == "Blue" then
+						maskequip("Diamond Mask")
+					end
+				else
+					maskequip("Gummy Mask")
+				end
+				lasttouched = v
+			end
+		end
+	end,
+	runningfor = 0,
+	oldtool = rtsg()["EquippedCollector"],
+	['gacf'] = function(part, st)
+		coordd = CFrame.new(part.Position.X, part.Position.Y+st, part.Position.Z)
+		return coordd
+	end
 }
 
 local planterst = {
-    plantername = {},
-    planterid = {}
+	plantername = {},
+	planterid = {}
 }
 
 for i,v in next, game:GetService("Workspace").MonsterSpawners:GetDescendants() do if v.Name == "TimerAttachment" then v.Name = "Attachment" end end
@@ -196,25 +196,25 @@ for i,v in pairs(getupvalues(require(game:GetService("ReplicatedStorage").Plante
 local donatableItemsTable = {}
 local treatsTable = {}
 for i,v in pairs(Items) do
-    if v.DonatableToWindShrine == true then
-        table.insert(donatableItemsTable,i)
-    end
+	if v.DonatableToWindShrine == true then
+		table.insert(donatableItemsTable,i)
+	end
 end
 
 for i,v in pairs(Items) do
-    if v.TreatValue then
-        table.insert(treatsTable,i)
-    end
+	if v.TreatValue then
+		table.insert(treatsTable,i)
+	end
 end
 
 local buffTable = {
-    "Blue Extract";
-    "Red Extract";
-    "Oil";
-    "Enzymes";
-    "Glue";
-    "Glitter";
-    "Tropical Drink";
+	"Blue Extract";
+	"Red Extract";
+	"Oil";
+	"Enzymes";
+	"Glue";
+	"Glitter";
+	"Tropical Drink";
 }
 
 table.sort(fieldstable)
@@ -257,95 +257,95 @@ antpart.CanCollide = false
 -- config
 
 local config = {
-    rares = {},
-    priority = {},
-    bestfields = {
-        red = "Pepper Patch",
-        white = "Coconut Field",
-        blue = "Stump Field"
-    },
-    blacklistedfields = {},
-    killerconfig = {},
-    bltokens = {},
-    toggles = {
-        autofarm = false,
-        farmclosestleaf = false,
-        farmbubbles = false,
-        autodig = false,
-        farmrares = false,
-        rgbui = false,
-        farmflower = false,
-        farmfuzzy = false,
-        farmcoco = false,
-        farmflame = false,
-        farmclouds = false,
-        killmondo = false,
-        killvicious = false,
-        loopspeed = false,
-        loopjump = false,
-        autoquest = false,
-        autoboosters = false,
-        autodispense = false,
-        clock = false,
-        freeantpass = false,
-        honeystorm = false,
-        autodoquest = false,
-        disableseperators = false,
-        npctoggle = false,
-        loopfarmspeed = false,
-        mobquests = false,
-        traincrab = false,
-        avoidmobs = false,
-        farmsprouts = false,
-        enabletokenblacklisting = false,
-        farmunderballoons = false,
-        farmsnowflakes = false,
-        collectgingerbreads = false,
-        collectcrosshairs = false,
-        farmpuffshrooms = false,
-        tptonpc = false,
-        donotfarmtokens = false,
-        convertballoons = false,
-        autostockings = false,
-        autosamovar = false,
-        autoonettart = false,
-        autocandles = false,
-        autofeast = false,
-        autoplanters = false,
-        autokillmobs = false,
-        autoant = false,
-        killwindy = false,
-        godmode = false,
-        disableconversion = false,
-        autodonate = false,
-    },
-    vars = {
-        field = "Ant Field",
-        convertat = 100,
-        farmspeed = 60,
-        prefer = "Tokens",
-        walkspeed = 70,
-        jumppower = 70,
-        npcprefer = "All Quests",
-        farmtype = "Walk",
-        monstertimer = 3,
-        autodigmode = "Normal",
-        donoItem = "Coconut",
-        donoAmount = 25,
-        selectedTreat = "Treat",
-        selectedTreatAmount = 0,
-    },
-    dispensesettings = {
-        blub = false,
-        straw = false,
-        treat = false,
-        coconut = false,
-        glue = false,
-        rj = false,
-        white = false,
-        red = false,
-        blue = false
-    }
+	rares = {},
+	priority = {},
+	bestfields = {
+		red = "Pepper Patch",
+		white = "Coconut Field",
+		blue = "Stump Field"
+	},
+	blacklistedfields = {},
+	killerconfig = {},
+	bltokens = {},
+	toggles = {
+		autofarm = false,
+		farmclosestleaf = false,
+		farmbubbles = false,
+		autodig = false,
+		farmrares = false,
+		rgbui = false,
+		farmflower = false,
+		farmfuzzy = false,
+		farmcoco = false,
+		farmflame = false,
+		farmclouds = false,
+		killmondo = false,
+		killvicious = false,
+		loopspeed = false,
+		loopjump = false,
+		autoquest = false,
+		autoboosters = false,
+		autodispense = false,
+		clock = false,
+		freeantpass = false,
+		honeystorm = false,
+		autodoquest = false,
+		disableseperators = false,
+		npctoggle = false,
+		loopfarmspeed = false,
+		mobquests = false,
+		traincrab = false,
+		avoidmobs = false,
+		farmsprouts = false,
+		enabletokenblacklisting = false,
+		farmunderballoons = false,
+		farmsnowflakes = false,
+		collectgingerbreads = false,
+		collectcrosshairs = false,
+		farmpuffshrooms = false,
+		tptonpc = false,
+		donotfarmtokens = false,
+		convertballoons = false,
+		autostockings = false,
+		autosamovar = false,
+		autoonettart = false,
+		autocandles = false,
+		autofeast = false,
+		autoplanters = false,
+		autokillmobs = false,
+		autoant = false,
+		killwindy = false,
+		godmode = false,
+		disableconversion = false,
+		autodonate = false,
+	},
+	vars = {
+		field = "Ant Field",
+		convertat = 100,
+		farmspeed = 60,
+		prefer = "Tokens",
+		walkspeed = 70,
+		jumppower = 70,
+		npcprefer = "All Quests",
+		farmtype = "Walk",
+		monstertimer = 3,
+		autodigmode = "Normal",
+		donoItem = "Coconut",
+		donoAmount = 25,
+		selectedTreat = "Treat",
+		selectedTreatAmount = 0,
+	},
+	dispensesettings = {
+		blub = false,
+		straw = false,
+		treat = false,
+		coconut = false,
+		glue = false,
+		rj = false,
+		white = false,
+		red = false,
+		blue = false
+	}
 }
 
 local defaultconfig = config
@@ -353,47 +353,47 @@ local defaultconfig = config
 function statsget() local StatCache = require(game.ReplicatedStorage.ClientStatCache) local stats = StatCache:Get() return stats end
 
 function farm(trying)
-    if config.toggles.loopfarmspeed then game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = config.vars.farmspeed end
-    api.humanoid():MoveTo(trying.Position) 
-    repeat task.wait() until (trying.Position-api.humanoidrootpart().Position).magnitude <=4 or not IsToken(trying) or not temptable.running
+	if config.toggles.loopfarmspeed then game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = config.vars.farmspeed end
+	api.humanoid():MoveTo(trying.Position) 
+	repeat task.wait() until (trying.Position-api.humanoidrootpart().Position).magnitude <=4 or not IsToken(trying) or not temptable.running
 end
 
 function disableall()
-    if config.toggles.autofarm and not temptable.converting then
-        temptable.cache.autofarm = true
-        config.toggles.autofarm = false
-    end
-    if config.toggles.killmondo and not temptable.started.mondo then
-        config.toggles.killmondo = false
-        temptable.cache.killmondo = true
-    end
-    if config.toggles.killvicious and not temptable.started.vicious then
-        config.toggles.killvicious = false
-        temptable.cache.vicious = true
-    end
-    if config.toggles.killwindy and not temptable.started.windy then
-        config.toggles.killwindy = false
-        temptable.cache.windy = true
-    end
+	if config.toggles.autofarm and not temptable.converting then
+		temptable.cache.autofarm = true
+		config.toggles.autofarm = false
+	end
+	if config.toggles.killmondo and not temptable.started.mondo then
+		config.toggles.killmondo = false
+		temptable.cache.killmondo = true
+	end
+	if config.toggles.killvicious and not temptable.started.vicious then
+		config.toggles.killvicious = false
+		temptable.cache.vicious = true
+	end
+	if config.toggles.killwindy and not temptable.started.windy then
+		config.toggles.killwindy = false
+		temptable.cache.windy = true
+	end
 end
 
 function enableall()
-    if temptable.cache.autofarm then
-        config.toggles.autofarm = true
-        temptable.cache.autofarm = false
-    end
-    if temptable.cache.killmondo then
-        config.toggles.killmondo = true
-        temptable.cache.killmondo = false
-    end
-    if temptable.cache.vicious then
-        config.toggles.killvicious = true
-        temptable.cache.vicious = false
-    end
-    if temptable.cache.windy then
-        config.toggles.killwindy = true
-        temptable.cache.windy = false
-    end
+	if temptable.cache.autofarm then
+		config.toggles.autofarm = true
+		temptable.cache.autofarm = false
+	end
+	if temptable.cache.killmondo then
+		config.toggles.killmondo = true
+		temptable.cache.killmondo = false
+	end
+	if temptable.cache.vicious then
+		config.toggles.killvicious = true
+		temptable.cache.vicious = false
+	end
+	if temptable.cache.windy then
+		config.toggles.killwindy = true
+		temptable.cache.windy = false
+	end
 end
 
 local AllTokens = game.Workspace.Collectibles
@@ -428,22 +428,22 @@ local DecalsId={
 	[1629649299]="Look",
 	[2319083910]="Spike",
 	[1442700745]="Rage",
-	
+
 	[1472425802]="BrownBearMorph",
 	[1472532912]="PolarBearMorph",
 	[1472580249]="PandaBearMorph",
 	[177997841]="GummybeeOne",
 	[4889322534]="FuzzyBomb",
 	[4519523935]="VectorTrig",
-	
+
 	[2028453802]="Blueberry",
 	[1471882621]="RoyalJelly",
 	[1952796032]="Pineapple",
 	[1952682401]="SunflowerSeed",
 	[4889470194]="GiftField",
 	[1472491940]="BlackBearMorph",
-	
-	
+
+
 	[1874692303]="BlueSyntez",
 	[10000000000000]="dddd",
 	[10000000000000]="dddd",
@@ -467,11 +467,11 @@ AllTokens.ChildAdded:Connect(function(child)
 	child.Name=SmallDecalsId[FrontDecal.Texture] or FullDecalsId[FrontDecal.Texture] or "C"
 	if child.Name=="C" then
 		print(FrontDecal.Texture,nil)
-    else
-        print(child.Name)
+	else
+		print(child.Name)
 	end
 
-    spawned_tokens[child]={token=child,spawned=os.clock(),name=child.Name}
+	spawned_tokens[child]={token=child,spawned=os.clock(),name=child.Name}
 end)
 
 AllTokens.ChildRemoved:Connect(function(child)
@@ -480,8 +480,8 @@ AllTokens.ChildRemoved:Connect(function(child)
 	if child.Name~="C" then
 		print(FrontDecal.Texture,nil)
 	end
-    if not spawned_tokens[child] then return end
-    print(spawned_tokens[child].Name,os.clock()-spawned_tokens[child].spawned)
+	if not spawned_tokens[child] then return end
+	print(spawned_tokens[child].Name,os.clock()-spawned_tokens[child].spawned)
 end)
 
 print(acces_key)
