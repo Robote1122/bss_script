7bcddb9dcf4ca51e697e3a987385eec8f4f0c1f3
Robote1122 <ivan.1m4f@yandex.ru>
2026-07-14 14:35:30 +0300

    Update

--- mve.lua
+++ mve.lua
@@ -359,6 +359,8 @@ function enableall()
     end
 end
 
+--[[
+
 local AllTokens = game.Workspace.Collectibles
 
 local spawned_tokens = {}
@@ -444,5 +446,5 @@ AllTokens.ChildRemoved:Connect(function(child)
     if not spawned_tokens[child] return end
     print(spawned_tokens[child].Name,os.clock()-spawned_tokens[child].spawned)
 end)
-
+]]
 print(acces_key)
